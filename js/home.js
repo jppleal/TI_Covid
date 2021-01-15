7fadb1b922f2cf871f101405c24f4d7d6652776e
José Leal <34638627+jppleal@users.noreply.github.com>
2021-01-15 03:25:44 +0000

    10º commit - Home concluído
    Todas as funcionalidades da página home.html concluídas a nível de jQuery em home.js
    
    CSS foi distribuido em ficheiros .css diferentes por questões de organização

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     //variável que armazena a response
     var settings = {
@@ -7,18 +7,67 @@ $(document).ready(function () {
         "timeout": 0,
     };
 
-    dadosMundo();
+    dadosMundo(); // executa logo a função dadosMundo
+
+    //cria uma variável para guardar e excutar um setInterval da função dadosVariosPaises() de 3 em 3 segundos
+    var interval = setInterval(function() {
+        dadosVariosPaises();
+    }, 3000);;
+
 
     //preenche os campos dos números do covid a nível mundial
-    function dadosMundo(){
-
-        $.ajax(settings).done(function (response) {
+    function dadosMundo() {
+        $.ajax(settings).done(function(response) {
             var content_mundo = response.Global;
-            document.getElementById('total_confirmados').innerHTML = content_mundo.TotalConfirmed + '<span id="novos_confirmados"> (+ '+content_mundo.NewConfirmed + ') </span>';
-            document.getElementById('total_recuperados').innerHTML = content_mundo.NewConfirmed + '<span id="novos_recuperados"> (+ '+content_mundo.NewRecovered + ') </span>';
-            document.getElementById('total_mortos').innerHTML = content_mundo.NewDeaths + '<span id="novos_mortos"> (+ '+content_mundo.NewDeaths + ') </span>';
+            document.getElementById('total_confirmados').innerHTML = content_mundo.TotalConfirmed + '<span id="novos_confirmados"> (+ ' + content_mundo.NewConfirmed + ') </span>';
+            document.getElementById('total_recuperados').innerHTML = content_mundo.NewConfirmed + '<span id="novos_recuperados"> (+ ' + content_mundo.NewRecovered + ') </span>';
+            document.getElementById('total_mortos').innerHTML = content_mundo.NewDeaths + '<span id="novos_mortos"> (+ ' + content_mundo.NewDeaths + ') </span>';
         });
     };
 
+    //obter o país consoante a pesquisa a ser feita no <input>
+    $('#nome_search_pais').on('input', function() {
+        $.ajax(settings).done(function(response) {
+            var content_pais = response.Countries; //vai buscar a array "Countries" à resposta do pedido na variável settings
+            var input_text = $('#nome_search_pais').val().toUpperCase(); //guarda o valor no <input> em maísculas
 
+            for (var i = 0; i < content_pais.length; i++) { //um ciclo que passa por todas as linhas da array
+                var row = content_pais[i]; //na linha do array
+                if (row.Country.toUpperCase().startsWith(input_text)) { //transforma o nome do país em maisculas e compara o começo com o valor do <input>
+                    $("#nome_pais").text(row.Country); //o nome do país
+                    $("#lista_total_confirmados").text(row.TotalConfirmed); //total de casos confirmados
+                    $("#lista_novos_confirmados").text('(+' + row.NewConfirmed + ')'); //total de novos casos confirmados
+                    $("#lista_total_recuperados").text(row.TotalRecovered); //total de casos recuperados
+                    $("#lista_novos_recuperados").text('(+' + row.NewRecovered + ')'); //total de novos casos recuperados
+                    $("#lista_total_mortos").text(row.TotalDeaths); //total de mortes confirmadas
+                    $("#lista_novos_mortos").text('(+' + row.NewDeaths + ')'); //total de novas mortes confirmadas
+
+                    clearInterval(interval); // uma vez que existe valor no <input> e existe um resultado, ele para o setInterval para impedir que o resultado da pesquisa desapareça
+                }
+                if (input_text == "") { //quando se apaga o que está dentro do <input>
+
+                    interval = setInterval(function() { //executa novamente a função dadosVariosPaises() de 3 em 3 segundos
+                        dadosVariosPaises();
+                    }, 3000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    function dadosVariosPaises() { //vai buscar um país aleatorio para preencher os campos por baixo do <input>, quando não existe resultado de pesquisa
+        $.ajax(settings).done(function(response) {
+            var content_pais = response.Countries;
+            var i = Math.floor(Math.random() * 188); //calcula um valor aleatório
+            var row = content_pais[i];
+            $("#nome_pais").text(row.Country);
+            $("#lista_total_confirmados").text(row.TotalConfirmed);
+            $("#lista_novos_confirmados").text('(+' + row.NewConfirmed + ')');
+            $("#lista_total_recuperados").text(row.TotalRecovered);
+            $("#lista_novos_recuperados").text('(+' + row.NewRecovered + ')');
+            $("#lista_total_mortos").text(row.TotalDeaths);
+            $("#lista_novos_mortos").text('(+' + row.NewDeaths + ')');
+
+        })
+    }
 });
--- js/newsletter.js
+++ js/newsletter.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $(".table_card").css("display", "none");
 
     //Variável que armazena a response 
@@ -9,7 +9,7 @@ $(document).ready(function () {
     };
 
     //Preenche a select list
-    $.ajax(settings).done(function (response) {
+    $.ajax(settings).done(function(response) {
         var content = response.Countries;
         for (var i = 0; i < content.length; i++) {
             var row = content[i];
@@ -18,7 +18,7 @@ $(document).ready(function () {
     });
 
     //função que executa ao mudar de país
-    $(document).on('change', 'select', function () {
+    $(document).on('change', 'select', function() {
         var dd = $(this).val(); //obtém o valor da opção selecionada
 
         //Verifica se um país foi selecionado para mostrar ou não os dados do país
@@ -29,17 +29,16 @@ $(document).ready(function () {
         }
 
         //Preenche os campos pelo id
-        $.ajax(settings).done(function (response) {
+        $.ajax(settings).done(function(response) {
             var content = response.Countries;
             for (var i = 0; i < content.length; i++) {
-                var row = content[i]
-
+                var row = content[i];
                 if (row.Slug == dd) {
                     $("#nome_pais").text(row.Country);
                     $("#total_confirmados").text(row.TotalConfirmed);
-                    $("#novos_confirmados").text('(+' +row.NewConfirmed + ')');
+                    $("#novos_confirmados").text('(+' + row.NewConfirmed + ')');
                     $("#total_recuperados").text(row.TotalRecovered);
-                    $("#novos_recuperados").text('(+' + row.NewRecovered +')');
+                    $("#novos_recuperados").text('(+' + row.NewRecovered + ')');
                     $("#total_mortos").text(row.TotalDeaths);
                     $("#novos_mortos").text('(+' + row.NewDeaths + ')');
                 }
